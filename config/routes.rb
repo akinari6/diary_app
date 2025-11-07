234968b9c3285e926398c435c4635152facef741
atsuji <ttakinai@yahoo.co.jp>
2025-11-07 23:18:41 +0900

    feat: add diary crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :diaries
+  resources :users, only: [:index, :show, :edit, :update]
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,5 +14,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "diaries#index"
 end
